fc95f49aa96f5013c040158d3483452b8619167f
João Victor <themaciel190@gmail.com>
2024-06-09 22:14:24 -0500

    login completion and adding helper folder to help with the request

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,13 +1,56 @@
-import login from "@/actions/login";
+"use client";
 
-export default function LoginForm() {
+import login from "@/actions/login";
+import { useFormState, useFormStatus } from "react-dom";
+import Button from "@/components/forms/button";
+import Input from "../forms/input";
+import ErrorMessage from "../forms/error-message";
+import React from "react";
+import Link from "next/link";
+import styles from "./login-form.module.css";
+
+function FormButton() {
+  const { pending } = useFormStatus();
   return (
     <>
-      <form action={login}>
-        <input type="text" name="username" placeholder="usuário" />
-        <input type="password" name="password" placeholder="senha" />
-        <button>Entrar</button>
+      {pending ? (
+        <Button disabled={pending}>Carregando...</Button>
+      ) : (
+        <Button disabled={pending}>Entrar</Button>
+      )}
+    </>
+  );
+}
+
+export default function LoginForm() {
+  const [state, action] = useFormState(login, {
+    ok: false,
+    error: "",
+    data: null,
+  });
+
+  React.useEffect(() => {
+    if (state.ok) window.location.href = "/conta";
+  }, [state.ok]);
+
+  return (
+    <>
+      <form action={action} className={styles.form}>
+        <Input label="Usuário" name="username" type="text" />
+        <Input label="Senha" name="password" type="password" />
+        <ErrorMessage error={state.error} />
+        <FormButton />
       </form>
+      <Link href={"/login/perdeu"} className={styles.perdeu}>
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastra-se</h2>
+        <p>Ainda não possui conta? Cadastra-se no site.</p>
+        <Link href="/login/criar" className="button">
+          Cadastro
+        </Link>
+      </div>
     </>
   );
 }
